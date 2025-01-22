f93f3ecee1675e3b818ec0f4ca5c25ec976e196d
Baehyeonu <wea3113@gmail.com>
2025-01-22 15:00:23 +0900

    🌱 주석 제거

--- chapter37~39/sakila.sql
+++ chapter37~39/sakila.sql
@@ -1,124 +1,124 @@
 -- 데이터 조회 및 필터링
 -- 특정 배우가 출연한 영화 목록 조회
--- SELECT f.film_id, f.title
--- FROM film f
--- JOIN film_actor fa ON f.film_id = fa.film_id
--- JOIN actor a ON fa.actor_id = a.actor_id
--- WHERE a.first_name = 'PENELOPE' AND a.last_name = 'GUINESS';
+SELECT f.film_id, f.title
+FROM film f
+JOIN film_actor fa ON f.film_id = fa.film_id
+JOIN actor a ON fa.actor_id = a.actor_id
+WHERE a.first_name = 'PENELOPE' AND a.last_name = 'GUINESS';
 
 -- 모든 카테고리와 해당 카테고리의 영화 수 조회
--- SELECT c.name, COUNT(fc.film_id) as number_of_films
--- FROM category c
--- JOIN film_category fc ON c.category_id = fc.category_id
--- GROUP BY c.name;
+SELECT c.name, COUNT(fc.film_id) as number_of_films
+FROM category c
+JOIN film_category fc ON c.category_id = fc.category_id
+GROUP BY c.name;
 
 -- 특정 고객의 대여 기록 조회
--- SELECT r.rental_date, f.title
--- FROM rental r
--- JOIN inventory i on r.inventory_id = i.inventory_id
--- JOIN film f on i.film_id = f.film_id
--- WHERE r.customer_id=5;
+SELECT r.rental_date, f.title
+FROM rental r
+JOIN inventory i on r.inventory_id = i.inventory_id
+JOIN film f on i.film_id = f.film_id
+WHERE r.customer_id=5;
 
 -- 가장 최근에 추가된 10개의 영화 조회
--- SELECT title, release_year
--- FROM film
--- ORDER BY release_year DESC
--- LIMIT 10;
+SELECT title, release_year
+FROM film
+ORDER BY release_year DESC
+LIMIT 10;
 
 -- 조인 및 관계
 -- 특정 영화에 출연한 배우 목록 조회
--- SELECT a.first_name, a.last_name
--- FROM actor a
--- JOIN film_actor fa ON a.actor_id = fa.actor_id
--- JOIN film f ON fa.film_id = f.film_id
--- WHERE f.title = 'ACADEMY DINOSAUR';
+SELECT a.first_name, a.last_name
+FROM actor a
+JOIN film_actor fa ON a.actor_id = fa.actor_id
+JOIN film f ON fa.film_id = f.film_id
+WHERE f.title = 'ACADEMY DINOSAUR';
 
 -- 특정 영화를 대여한 고객 목록 조회
--- SELECT DISTINCT c.first_name, c.last_name
--- FROM customer c
--- JOIN rental r ON c.customer_id = r.customer_id
--- JOIN inventory i ON r.inventory_id = i.inventory_id
--- JOIN film f ON i.film_id = f.film_id
--- WHERE f.title = 'ACADEMY DINOSAUR';
+SELECT DISTINCT c.first_name, c.last_name
+FROM customer c
+JOIN rental r ON c.customer_id = r.customer_id
+JOIN inventory i ON r.inventory_id = i.inventory_id
+JOIN film f ON i.film_id = f.film_id
+WHERE f.title = 'ACADEMY DINOSAUR';
 
 -- 모든 고객과 그들이 가장 최근에 대여한 영화 조회
--- SELECT c.customer_id, c.first_name, c.last_name, MAX(r.rental_date) as last_rental_date, f.title
--- FROM customer c
--- JOIN rental r ON c. customer_id = r.customer_id
--- JOIN inventory i ON r.inventory_id = i.inventory_id
--- JOIN film f ON i.film_id = f.film_id
--- GROUP BY c.customer_id, c.first_name, c.last_name, f.title
+SELECT c.customer_id, c.first_name, c.last_name, MAX(r.rental_date) as last_rental_date, f.title
+FROM customer c
+JOIN rental r ON c. customer_id = r.customer_id
+JOIN inventory i ON r.inventory_id = i.inventory_id
+JOIN film f ON i.film_id = f.film_id
+GROUP BY c.customer_id, c.first_name, c.last_name, f.title
 
 -- 각 영화별 평균 대여 기간 조회
--- SELECT f.title, AVG(DATEDIFF(r.return_date, r.rental_date)) as avg_rental_preiod
--- FROM film f
--- JOIN inventory i ON f.film_id = i.film_id
--- JOIN rental r ON i.inventory_id = r.inventory_id
--- GROUP BY f.title
+SELECT f.title, AVG(DATEDIFF(r.return_date, r.rental_date)) as avg_rental_preiod
+FROM film f
+JOIN inventory i ON f.film_id = i.film_id
+JOIN rental r ON i.inventory_id = r.inventory_id
+GROUP BY f.title
 
 -- 집계 및 그룹화
 -- 가장 많이 대여된 영화 조회
--- SELECT f.title, COUNT(r.rental_id) as rental_count
--- FROM film f
--- JOIN inventory i on f.film_id = i.film_id
--- JOIN rental r ON i.inventory_id = r.inventory_id
--- GROUP BY f.title
--- ORDER BY rental_count DESC
--- LIMIT 1;
+SELECT f.title, COUNT(r.rental_id) as rental_count
+FROM film f
+JOIN inventory i on f.film_id = i.film_id
+JOIN rental r ON i.inventory_id = r.inventory_id
+GROUP BY f.title
+ORDER BY rental_count DESC
+LIMIT 1;
 
 -- 각 카테고리별 평균 대여 요금 조회
--- SELECT c.name, AVG(f.rental_rate) as average_rental_rate
--- FROM category c
--- JOIN film_category fc ON c.category_id = fc.category_id
--- JOIN film f ON fc.film_id = f.film_id
--- GROUP BY c.name;
+SELECT c.name, AVG(f.rental_rate) as average_rental_rate
+FROM category c
+JOIN film_category fc ON c.category_id = fc.category_id
+JOIN film f ON fc.film_id = f.film_id
+GROUP BY c.name;
 
 -- 월별 총 매출 조회
--- SELECT YEAR(p.payment_date) as year, MONTH(p.payment_date) as month, SUM(p.amount) as total_sales
--- FROM payment p
--- GROUP BY YEAR(p.payment_date), MONTH(p.payment_date)
+SELECT YEAR(p.payment_date) as year, MONTH(p.payment_date) as month, SUM(p.amount) as total_sales
+FROM payment p
+GROUP BY YEAR(p.payment_date), MONTH(p.payment_date)
 
 -- 각 배우별 출연한 영화 수 조회
--- SELECT a.first_name, a.last_name, COUNT(fa.film_id) as numver_of_films
--- FROM actor a
--- JOIN film_actor fa ON a.actor_id = fa.actor_id
--- GROUP BY a.first_name, a.last_name;
+SELECT a.first_name, a.last_name, COUNT(fa.film_id) as numver_of_films
+FROM actor a
+JOIN film_actor fa ON a.actor_id = fa.actor_id
+GROUP BY a.first_name, a.last_name;
 
 -- 서브쿼리 및 고급 기능
 -- 가장 수익이 많은 영화 조회
--- SELECT f.title, SUM(p.amount) AS total_revenue
--- FROM film f
--- JOIN inventory i ON f.film_id = i.film_id
--- JOIN rental r ON i.inventory_id = r.inventory_id
--- JOIN payment p ON r.rental_id=p.rental_id
--- GROUP BY f.title
--- ORDER BY total_revenue DESC
--- LIMIT 1;
+SELECT f.title, SUM(p.amount) AS total_revenue
+FROM film f
+JOIN inventory i ON f.film_id = i.film_id
+JOIN rental r ON i.inventory_id = r.inventory_id
+JOIN payment p ON r.rental_id=p.rental_id
+GROUP BY f.title
+ORDER BY total_revenue DESC
+LIMIT 1;
 
 -- 평균 대여 요금보다 높은 요금의 영호하 조회
--- SELECT f.title, f.rental_rate
--- FROM film f
--- WHERE f.rental_rate > (SELECT AVG(rental_rate) FROM film);
+SELECT f.title, f.rental_rate
+FROM film f
+WHERE f.rental_rate > (SELECT AVG(rental_rate) FROM film);
 
 -- 가장 활동적인 고객 조회(가장 많은 영화를 대여한 고객 찾기)
--- SELECT c.customer_id, c.first_name, c.last_name, COUNT(r.rental_id) AS rental_count
--- FROM rental r
--- JOIN customer c ON r.customer_id = c.customer_id
--- GROUP BY c.customer_id
--- ORDER BY rental_count DESC
--- LIMIT 1;
+SELECT c.customer_id, c.first_name, c.last_name, COUNT(r.rental_id) AS rental_count
+FROM rental r
+JOIN customer c ON r.customer_id = c.customer_id
+GROUP BY c.customer_id
+ORDER BY rental_count DESC
+LIMIT 1;
 
 -- 특정 배우가 출연한 영화 중 가장 인기 있는 영화조회
--- SELECT f.title, COUNT(r.rental_id) AS rental_count
--- FROM film f
--- JOIN film_actor fa ON f.film_id = fa.film_id
--- JOIN actor a ON fa.actor_id = a.actor_id
--- JOIN inventory i ON f.film_id = i.film_id
--- JOIN rental r ON i.inventory_id = r.inventory_id
--- WHERE a.first_name = 'PENELOPE' AND a.last_name = 'GUINESS'
--- GROUP BY f.title
--- ORDER BY rental_count DESC
--- LIMIT 1;
+SELECT f.title, COUNT(r.rental_id) AS rental_count
+FROM film f
+JOIN film_actor fa ON f.film_id = fa.film_id
+JOIN actor a ON fa.actor_id = a.actor_id
+JOIN inventory i ON f.film_id = i.film_id
+JOIN rental r ON i.inventory_id = r.inventory_id
+WHERE a.first_name = 'PENELOPE' AND a.last_name = 'GUINESS'
+GROUP BY f.title
+ORDER BY rental_count DESC
+LIMIT 1;
 
 -- 데이터 수정 및 관리
 -- 새로운 영화 추가
